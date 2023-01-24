cd87686e4d55bb535eb64dd495361db4fcf701be
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2023-01-23 20:02:24 -0500

    Some update to ppm_parsing

--- ppm_parsing/dist/src/ppm.rs
+++ ppm_parsing/dist/src/ppm.rs
@@ -5,38 +5,21 @@ pub struct PPM {
 }
 
 enum ParseItem {
-    /// If the next 3 tokens are 'P6\n', go to WhitespaceToNext(Width). Otherwise, terminate with error
     Magic,
-    /// While in this state, parse the width as base-10 ASCII digits before going to
-    /// WhitespaceToNext(Height)
     Width,
-    /// While in this state, parse the height as base-10 ASCII digits before going to
-    /// WhitespaceToNext(Maxval)
     Height,
-    /// While in this state, parse the "maxval" (maximum intensity of any pixel channel) as
-    /// base-10 ASCII digits before going to NewlineToPixels
     Maxval,
-    /// The pixels are stored as raw bytes, so we don't need to parse them; this is a terminal
-    /// state
     Pixels,
 }
 enum ParseState {
-    /// While in this state, the current item is being parsed
     Parsing(ParseItem),
-    /// While in this state, skip all whitespace and comments. If the current token is
-    /// whitespace, stay in this state. If the current token is the beginning of a comment,
-    /// transition to CommentToNext(ParseItem). Otherwise, trasition to Parsing(ParseItem).
     WhitespaceToNext(ParseItem),
-    /// While in this state, skip until the end of a comment, denoted by a newline. Once a
-    /// newline character is found, transition to WhitespaceToNext(ParseItem). Otherwise, stay
-    /// in this state.
     CommentToNext(ParseItem),
 }
 
 pub fn parse(buf: &[u8]) -> Result<PPM, &'static str> {
     // Read up on the PPM format at http://ailab.eecs.wsu.edu/wise/P1/PPM.html
-    // We will be using the P6 version, i.e. pixels stored as raw bytes, for conveniece (you just
-    // need to parse the header)
+    // We will be using the P6 version, i.e. pixels stored as raw bytes, for conveniece
 
     let mut width = 0u32;
     let mut height = 0u32;
@@ -52,20 +35,55 @@ pub fn parse(buf: &[u8]) -> Result<PPM, &'static str> {
         let c = buf[index];
 
         match state {
-            ParseState::Parsing(ParseItem::Pixels) => break,
-            ParseState::Parsing(_) => todo!(),
-            ParseState::WhitespaceToNext(_) => todo!(),
-            ParseState::CommentToNext(_) => todo!(),
+            ParseState::Parsing(ParseItem::Magic) => {
+                // If the next 3 tokens are 'P6\n', go to WhitespaceToNext(Width). Otherwise, terminate with error
+                todo!()
+            }
+            ParseState::Parsing(ParseItem::Width) => {
+                // While in this state, parse the width as base-10 ASCII digits before going to
+                // WhitespaceToNext(Height)
+                todo!()
+            }
+            ParseState::Parsing(ParseItem::Height) => {
+                // While in this state, parse the height as base-10 ASCII digits before going to
+                // WhitespaceToNext(Maxval)
+                todo!()
+            }
+            ParseState::Parsing(ParseItem::Maxval) => {
+                // While in this state, parse the "maxval" (maximum intensity of any pixel channel) as
+                // base-10 ASCII digits, and check that the next non-digit byte is a newline before
+                // going to Parsing(Pixels)
+                todo!()
+            }
+            ParseState::Parsing(ParseItem::Pixels) => {
+                // The pixels are stored as raw bytes, so we don't need to parse them; this is a terminal
+                // state
+                break;
+            }
+            ParseState::WhitespaceToNext(_) => {
+                // While in this state, skip all whitespace and comments. If the current token is
+                // whitespace, stay in this state. If the current token is the beginning of a comment,
+                // transition to CommentToNext(ParseItem). Otherwise, trasition to Parsing(ParseItem).
+                todo!()
+            }
+            ParseState::CommentToNext(_) => {
+                // While in this state, skip until the end of a comment, denoted by a newline. Once a
+                // newline character is found, transition to WhitespaceToNext(ParseItem). Otherwise, stay
+                // in this state.
+                todo!()
+            }
         }
     }
 
     let pixels = buf[index..]
         .chunks_exact(3)
-        .skip(1)
-        .map(|p| (p[1], p[2], p[0]))
+        .map(|p| (p[0], p[1], p[2]))
         .collect::<Vec<_>>();
 
-    assert!(pixels.len() == (width * height) as usize);
+    if pixels.len() != (width * height) as usize {
+        println!("{} != {}", pixels.len(), width * height);
+        return Err("Number of pixels does not match dimensions");
+    }
 
     Ok(PPM {
         width,
