cccb8ff90f8cb4d48e4a7b2c870dccabf1ee57d2
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2023-03-15 22:37:12 -0400

    WIP: derive_map refsol is in the dist directory
    and it still has a lot of rough edges and is inconsistent with the
    readme but a macro like this is very possible and only took me like
    1.2hr

--- derive_map/dist/derive_map/src/lib.rs
+++ derive_map/dist/derive_map/src/lib.rs
@@ -1,14 +1,75 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub use derive_map_proc::*;
+
+pub trait Map {
+    type Item;
+    /// Do a map operation, calling `f` on all values of type `Item` contained
+    /// in `self` and replacing them in-place
+    fn map_inplace(&mut self, f: &mut impl FnMut(&mut Self::Item));
+
+    /*
+    /// Map operation that takes ownership during the map
+    fn map(self, f: impl FnMut(Self::Item) -> Self::Item) -> Self
+    where
+        Self: Sized;
+    */
+}
+
+impl<T> Map for Vec<T> {
+    type Item = T;
+    fn map_inplace(&mut self, f: &mut impl FnMut(&mut Self::Item)) {
+        self.iter_mut().for_each(f)
+    }
+
+    /*
+    fn map(self, f: impl FnMut(Self::Item) -> Self::Item) -> Self {
+        todo!()
+    }
+    */
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[derive(PartialEq, Debug)]
+    struct Vec3 {
+        x: f32,
+        y: f32,
+        z: f32,
+    }
+
+    impl Map for Vec3 {
+        type Item = f32;
+        fn map_inplace(&mut self, f: &mut impl FnMut(&mut f32)) {
+            f(&mut self.x);
+            f(&mut self.y);
+            f(&mut self.z);
+        }
+    }
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_simple_vec3_map() {
+        let mut v = Vec3 {
+            x: 1.0f32,
+            y: 2.0f32,
+            z: 3.0f32,
+        };
+        v.map_inplace(&mut |a| *a = *a * 2.0f32);
+        let v2 = Vec3 {
+            x: 2.0f32,
+            y: 4.0f32,
+            z: 6.0f32,
+        };
+        assert_eq!(v, v2);
+    }
+
+    #[derive(Map, PartialEq, Debug)]
+    struct VecPair<T>(Vec<T>, Vec<T>);
+
+    #[test]
+    fn test_pair_vec_map() {
+        let mut v = VecPair(vec![0i32, 2i32], vec![5i32]);
+        v.map_inplace(&mut |f| *f = *f / 2);
+        let v2 = VecPair(vec![0i32, 1i32], vec![2i32]);
+        assert_eq!(v, v2);
     }
 }
--- derive_map/dist/derive_map_proc/src/lib.rs
+++ derive_map/dist/derive_map_proc/src/lib.rs
@@ -1,14 +1,87 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use proc_macro2::{self, TokenStream};
+use quote::{quote, quote_spanned, TokenStreamExt};
+use syn::{self, parse_macro_input, spanned::Spanned, DeriveInput, TypeParam};
+
+#[proc_macro_derive(Map)]
+pub fn derive_map(annotated_item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let derive_input = parse_macro_input!(annotated_item);
+    derive_map_impl(derive_input).into()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn derive_map_impl(annotated_item: DeriveInput) -> TokenStream {
+    let DeriveInput {
+        ident,
+        generics,
+        data,
+        ..
+    } = annotated_item;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    let struct_data = match data {
+        syn::Data::Struct(s) => s,
+        _ => {
+            return quote_spanned! {
+                ident.span() => compile_error!("#[derive(Map)] can only be applied to `struct`!");
+            }
+        }
+    };
+
+    let assign_all_fields = match struct_data.fields {
+        syn::Fields::Unit => quote! {},
+        syn::Fields::Unnamed(uf) => {
+            let assignments = (0..uf.unnamed.len()).map(|i| {
+                let i = syn::Index::from(i);
+                quote! {
+                    self.#i.map_inplace(f);
+                }
+            });
+            let mut all_assignments = TokenStream::new();
+            all_assignments.append_all(assignments);
+            all_assignments
+        }
+        syn::Fields::Named(nf) => {
+            let assignments = nf.named.iter().map(|f| {
+                let field_name = f.ident.as_ref().unwrap();
+                quote! {
+                    self.#field_name.map_inplace(f);
+                }
+            });
+            let mut all_assignments = TokenStream::new();
+            all_assignments.append_all(assignments);
+            all_assignments
+        }
+    };
+
+    let mut type_param = Option::<TypeParam>::None;
+    for param in generics.params.iter() {
+        if let syn::GenericParam::Type(tp) = param {
+            match type_param {
+                None => type_param = Some(tp.clone()),
+                Some(_) => {
+                    return quote_spanned! {
+                        generics.span() => compile_error!("#[derive(Map)] cannot be applied to structs with more than one type parameter");
+                    }
+                }
+            }
+        }
     }
+
+    let type_param = match type_param {
+        Some(tp) => tp,
+        None => {
+            return quote_spanned! {
+                ident.span() => compile_error!("#[derive(Map)] must be applied to a struct with a single type parameter");
+            }
+        }
+    };
+
+    let output = quote! {
+        impl #generics Map for #ident #generics {
+            type Item = #type_param;
+            fn map_inplace(&mut self, f: &mut impl FnMut(&mut Self::Item)) {
+                #assign_all_fields
+            }
+        }
+    };
+
+    output.into()
 }
