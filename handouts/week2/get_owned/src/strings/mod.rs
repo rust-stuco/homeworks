6028d39cbd7c1017a7e391b281228a679806dd99
Connor Tsui <connor.tsui20@gmail.com>
2023-09-28 15:23:40 -0400

    remove handouts for now

--- handouts/week2/get_owned/src/strings/mod.rs
+++
@@ -1,9 +0,0 @@
-/// All examples taken from rustlings move_semantics.rs
-/// https://github.com/rust-lang/rustlings/tree/main/exercises/move_semantics
-
-/// Make sure you read ALL of the error messages!
-
-mod fixme1;
-// mod fixme2;
-// mod fixme3;
-// mod fixme4;
