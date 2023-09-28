7569adbea4a7c105e75c89b57b5855198529daf8
Connor Tsui <connor.tsui20@gmail.com>
2023-09-28 10:40:50 -0400

    added base week 2

---
+++ solutions/week2/get_owned/src/move_semantics/mod.rs
@@ -0,0 +1,11 @@
+/// All examples taken from rustlings move_semantics.rs
+/// https://github.com/rust-lang/rustlings/tree/main/exercises/move_semantics
+
+/// Make sure you read ALL of the error messages!
+
+// mod fixme1;
+// mod fixme2;
+// mod fixme3;
+// mod fixme4;
+// mod fixme5;
+// mod fixme6;
