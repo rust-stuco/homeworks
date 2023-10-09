06528bf94b604be7386fb84e835dc6535e4005d1
Connor Tsui <87130162+Connortsui20@users.noreply.github.com>
2023-10-09 08:42:56 -0400

    Hw2 replace questionmark (#1)
    replaced ??? with todos

--- 2-get_owned/src/strings/strings3.rs
+++ 2-get_owned/src/strings/strings3.rs
@@ -7,18 +7,15 @@
 
 
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    todo!("Remove whitespace from both ends of a string!")
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    todo!("Add 'world!' to the string! There's multiple ways to do this!")
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    todo!("Replace 'cars' in the string with 'balloons'!")
 }
 
 #[cfg(test)]
