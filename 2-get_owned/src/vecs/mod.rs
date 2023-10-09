9d8a0f0bc933f736de6bf635a74c6bb4ddd81e1c
Connor Tsui <connor.tsui20@gmail.com>
2023-10-09 08:00:11 -0400

    undo

--- 2-get_owned/src/vecs/mod.rs
+++
@@ -1,9 +0,0 @@
-// All examples taken from rustlings exercises
-// https://github.com/rust-lang/rustlings/tree/main/exercises/vecs
-
-// Uncomment each of these and ensure that it compiles with `cargo test -- vecs`.
-// We recommend uncommenting them one at a time.
-// Make sure you read ALL of the error messages! They contain VERY useful information.
-
-// mod vecs1;
-// mod vecs2;
