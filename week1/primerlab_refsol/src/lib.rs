833efcd42bfea6a40cd4d5a5b025dd1dac3a3585
Connor Tsui <connor.tsui20@gmail.com>
2023-11-25 16:35:46 -0500

    change writeup

--- week1/primerlab_refsol/src/lib.rs
+++ week1/primerlab_refsol/src/lib.rs
@@ -1,5 +1,7 @@
 //! ### 98-008: Intro to the Rust Programming Language
 //!
+//!
+//!
 //! # Primer Lab
 //!
 //! The goal of this homework is to make sure that you understand Rust's basic syntax.
@@ -12,6 +14,8 @@
 //! so if you are spending much more than that, please let us know!
 //! This time limit does not include setting up Rust on your machine though,
 //! so if you have trouble with installing Rust, make sure to ask for help.
+//!
+//!
 //!
 //! # Setup
 //!
@@ -39,7 +43,9 @@
 //! ```
 //!
 //! `cargo doc` will generate an HTML file that you can view in your browser!
-//! It might be easier to read the markdown-rendered docs than the comments in the handout.
+//! It might be easier to read the markdown-rendered docs than the comments in this handout.
+//!
+//!
 //!
 //! # Part 1: Exercises
 //!
@@ -48,50 +54,63 @@
 //!
 //! Run this command in the root of your project (parent directory of the `src/` folder):
 //! ```sh
-//! $ cargo test -- fixme
+//! $ cargo test
 //! ```
 //! **What does the error say?** Go into `src/exercises/fixme1.rs` and make the change.
 //!
-//! When you are able to run `cargo test -- fixme` without any errors,
+//! When you are able to run `cargo test` without any errors,
 //! move on to the next exercise by uncommenting `mod fixme2` in `src/exercises/mod.rs`.
-//! Go through all 8 of the exercises and
-//! make sure that the 8 test cases pass when you run `cargo test -- fixme`.
+//! Go through all 8 of the exercises by uncommenting each of the `pub mod fixme_;`s,
+//! and make sure that all the `fixme_` test cases pass when you run `cargo test`.
+//!
+//! At this point, you'll see other tests failing, which is what you'll fix in the next section.
+//!
 //!
 //!
 //! # Part 2: Function implementations
 //!
 //! For this part, you will need to implement 4 simple functions in Rust.
-//! In `src/lib.rs`, you will find 4 functions with a `todo!()` inside of them.
+//! In `src/functions.rs`, you will find 4 functions with a `todo!()` inside of them.
 //! Replace that `todo!()` with your implementation, according to the comment specification.
-//! One of the functions requires you to implement it in a certain way,
-//! so make sure to read those comments carefully.
+//!
+//! _One of the functions requires you to implement it in a certain way,_
+//! _so make sure to read those comments carefully._
 //!
 //! To test all of your functions, run:
 //! ```sh
 //! $ cargo test
 //! ```
-//! If you find that a test is running slowly, try running `cargo test --release`,
-//! which runs the tests in release mode. We'll talk about this more in future weeks,
-//! but basically it runs the tests with more compiler optimizations (like the `-O2` flag).
+//!
+//! If you find that a test is running slowly, run `cargo test --release`,
+//! which runs the tests in release mode. This runs the tests with
+//! compiler optimizations (like the `-O2` flag for C) and without debug symbols.
 //! Also, if you want to run a specific test, run `cargo test -- it_works`
 //! with the name of the test instead of `it_works`.
 //!
+//!
+//!
 //! # Submission
 //! - Run `make handin`
-//! - Submit the resultant `handin.tar` to Gradescope.
+//! - Submit the `handin.tar` that was generated to Gradescope.
 //!
 //! If you do not have `make` and `tar` installed on your system,
 //! install `make` and `tar` on your machine or use the CMU Linux SSH machines.
 //! If you need help with this, please reach out to us!
 //!
-//! ## Collaboration
+//!
+//!
+//! # Collaboration
+//!
 //! In general, feel free to discuss homeworks with other students!
 //! As long as you do not copy someone else's work, any communication is fair game.
-//! Feel free to discuss on the course Discord
-//! so that other students can see your questions and answers, as well.
+//! Try to discuss on the course Discord or Piazza so that
+//! other students can see your questions and answers as well!
+//!
+//!
 //!
 //! ## Feedback
-//! We would also like to reiterate that you should let us know if you spent
+//!
+//! We would like to reiterate that you should let us know if you spent
 //! anywhere in significant excess of an hour on this homework.
 //! These assignments are being deployed for the first time,
 //! and we are definitely open to feedback regarding the length and difficulty.
