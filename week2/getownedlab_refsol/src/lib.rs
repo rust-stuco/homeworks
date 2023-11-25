3504f6290d83ae651a6db2ce2eec311982cb9b46
Connor Tsui <connor.tsui20@gmail.com>
2023-11-25 17:19:46 -0500

    update hw2 docs

--- week2/getownedlab_refsol/src/lib.rs
+++ week2/getownedlab_refsol/src/lib.rs
@@ -1,5 +1,7 @@
 //! ### 98-008: Intro to the Rust Programming Language
 //!
+//!
+//!
 //! # Get Owned Lab
 //!
 //! The goal of this homework is to familiarize yourself with Rust's system of Ownership.
@@ -16,7 +18,9 @@
 //! it might be good to take a small break and just go over the borrow checker's rules.
 //!
 //! Remember to always read the error message that the compiler gives you, and as always,
-//! feel free to reach out for help!
+//! we encourage you to reach out for help!
+//!
+//!
 //!
 //! # Setup
 //!
@@ -32,40 +36,56 @@
 //! `cargo doc` will generate an HTML file that you can view in your browser!
 //! It might be easier to read the markdown-rendered docs than the comments in the handout.
 //!
+//!
+//!
 //! # Part 1: Move Semantics
 //!
-//! Like the previous homework, you will modify 6 files that don't pass the borrow checker
-//! by changing a few things to make them compile!
+//! Like the previous homework, you will modify 5 files that don't pass the borrow checker
+//! by changing a few things to make them compile! The first 3 exercises involve the `Vec` type,
+//! which is an _owned_ type.
+//!
+//! For this homework, all you need to know is the `vec![]` macro
+//! and the `push` method on vectors. Recall the `vec![]` creates a new vector with elements, and
+//! `push` appends an element onto the end of a vector.
 //!
 //! Run this command in the root of your project (parent directory of the `src/` folder):
 //! ```sh
-//! $ cargo test -- move_semantics
-//! ```
-//! **What does the error say?** Go into `src/exercises/fixme1.rs` and make the change.
-//!
-//! When you are able to run `cargo test -- fixme` without any errors,
-//! move on to the next exercise by uncommenting `mod fixme2` in `src/exercises/mod.rs`.
-//! Go through all 8 of the exercises and
-//! make sure that the 8 test cases pass when you run `cargo test -- fixme`.
-//!
-//!
-//! # Part 2: Function implementations
-//!
-//! For this part, you will need to implement 4 simple functions in Rust.
-//! In `src/lib.rs`, you will find 4 functions with a `todo!()` inside of them.
-//! Replace that `todo!()` with your implementation, according to the comment specification.
-//! One of the functions requires you to implement it in a certain way,
-//! so make sure to read those comments carefully.
-//!
-//! To test all of your functions, run:
-//! ```sh
 //! $ cargo test
 //! ```
-//! If you find that a test is running slowly, try running `cargo test --release`,
-//! which runs the tests in release mode. We'll talk about this more in future weeks,
-//! but basically it runs the tests with more compiler optimizations (like the `-O2` flag).
-//! Also, if you want to run a specific test, run `cargo test -- it_works`
-//! with the name of the test instead of `it_works`.
+//! **What does the error say?** Go into `src/move_semantics/move_semantics1.rs`
+//! and make the change.
+//!
+//! When you are able to run `cargo test` without any errors, move on to the next
+//! exercise by uncommenting `pub mod move_semantics2` in `src/move_semantics/mod.rs`.
+//! Go through all 5 of the exercises in `src/move_semantics` and
+//! make sure that all the test cases pass when you run `cargo test`.
+//!
+//!
+//!
+//! # Part 2: Strings
+//!
+//! In this section, you'll be working with `String` and `&str` instead of `Vec`.
+//!
+//!
+//! The first three exercises in `src/strings` are just like the previous section,
+//! where you need to make a change to pass the borrow checker and compile.
+//! Uncomment `pub mod strings1;` in `src/strings/mod.rs` and run `cargo test`.
+//! What does the compiler tell you? Make the change in `src/strings/strings1.rs` and then
+//! move onto the next exercises by uncommenting the other `pub mod strings_;`.
+//!
+//! A useful thing to remember here is that a `&String` can be _coerced_ into a `&str`.
+//! Other than that, just remember to make sure that you always read what the compiler tells you
+//! (it may or may not give away the answer)!
+//!
+//! For `strings4.rs`, you will need to look at documentation. You _could_ just google the answer,
+//! but it would be good to familiarize yourself with Rust documentation.
+//! Here is the official documentation for
+//! [`str`](https://doc.rust-lang.org/std/primitive.str.html).
+//!
+//! There's a search bar at the top of the documentation that you can also make use of.
+//! Try searching for methods called `trim` there!
+//!
+//!
 //!
 //! # Submission
 //! - Run `make handin`
@@ -75,14 +95,18 @@
 //! install `make` and `tar` on your machine or use the CMU Linux SSH machines.
 //! If you need help with this, please reach out to us!
 //!
-//! ## Collaboration
+//!
+//!
+//! # Collaboration
 //! In general, feel free to discuss homeworks with other students!
 //! As long as you do not copy someone else's work, any communication is fair game.
 //! Feel free to discuss on the course Discord
 //! so that other students can see your questions and answers, as well.
 //!
-//! ## Feedback
-//! We would also like to reiterate that you should let us know if you spent
+//!
+//!
+//! # Feedback
+//! We would like to reiterate that you should let us know if you spent
 //! anywhere in significant excess of an hour on this homework.
 //! These assignments are being deployed for the first time,
 //! and we are definitely open to feedback regarding the length and difficulty.
@@ -91,5 +115,5 @@
 //! you should reach out to us and let us know as well ---
 //! chances are, you're not the only one!
 
-mod move_semantics;
-mod strings;
+pub mod move_semantics;
+pub mod strings;
