0e1a4ffab8b448e6dbd0f9cb2f2945e505c0246e
Connor Tsui <connor.tsui20@gmail.com>
2023-11-28 23:30:45 -0500

    refsol done, docs not done

--- week3/pokelab_ref/src/pokemon/eevee.rs
+++ week3/pokelab_ref/src/pokemon/eevee.rs
@@ -21,11 +21,14 @@ pub struct Eevee {
     evolution: EeveeEvolution,
 }
 
+/// TODO: code examples on every method and redo some documentation
+
 /// `EeveeEvolution::Eevee` represents the starting state of an `Eevee`.
 /// Note that the struct `Eevee` is not the same as the enum variant `EeveeEvolution::Eevee`.
 /// Each of the 3 evolution states has an `f32` field that represents a multiplier on the
 /// pokemon's base stats. TODO code example
-enum EeveeEvolution {
+#[derive(PartialEq)]
+pub enum EeveeEvolution {
     Eevee,
     Vaporeon(f32),
     Jolteon(f32),
@@ -50,45 +53,62 @@ impl Eevee {
         }
     }
 
-    /// Get the type of the Eevee as a String.
-    /// ex: "Eevee", "Vaporeon", "Jolteon", "Flareon"
-    /// TODO code example
-    pub fn get_type(&self) -> String {
-        match self.evolution {
-
-        }
-    }
-
     /// Get the attack value of the Eevee.
     /// The attack value is calculated by multiplying the base attack
     /// by the Eevee state multiplier, and rounding
     pub fn get_attack(&self) -> f32 {
-        todo!()
+        match self.evolution {
+            EeveeEvolution::Eevee => self.attack as f32,
+            EeveeEvolution::Vaporeon(m) => self.attack as f32 * m,
+            EeveeEvolution::Jolteon(m) => self.attack as f32 * m,
+            EeveeEvolution::Flareon(m) => self.attack as f32 * m,
+        }
     }
 
     /// Get the defense value of the Eevee.
     /// The defense value is calculated by multiplying the base defense by the Eevee state multiplier.
     pub fn get_defense(&self) -> f32 {
-        todo!()
+        match self.evolution {
+            EeveeEvolution::Eevee => self.defense as f32,
+            EeveeEvolution::Vaporeon(m) => self.defense as f32 * m,
+            EeveeEvolution::Jolteon(m) => self.defense as f32 * m,
+            EeveeEvolution::Flareon(m) => self.defense as f32 * m,
+        }
     }
 
     /// Get the health value of the Eevee.
     /// The health value is calculated by multiplying the base health by the Eevee state multiplier.
     pub fn get_health(&self) -> f32 {
-        todo!()
+        match self.evolution {
+            EeveeEvolution::Eevee => self.health as f32,
+            EeveeEvolution::Vaporeon(m) => self.health as f32 * m,
+            EeveeEvolution::Jolteon(m) => self.health as f32 * m,
+            EeveeEvolution::Flareon(m) => self.health as f32 * m,
+        }
     }
 
     /// Get the level of the Eevee.
-    pub fn get_level(&self) -> f32 {
-        todo!()
+    pub fn get_level(&self) -> u16 {
+        self.level
     }
 
     /// Level up the Eevee by l. Preserves the rest of the state.
-    pub fn level_up(&mut self, l: usize) -> Eevee {
-        todo!()
+    pub fn level_up(&mut self, l: u16) {
+        self.level += l;
     }
 
-    /// Here we want to implement the `evolve` method for Eevee.
+    /// Get the type of the Eevee as a String.
+    /// ex: "Eevee", "Vaporeon", "Jolteon", "Flareon"
+    /// TODO code example
+    pub fn get_type(&self) -> String {
+        match self.evolution {
+            EeveeEvolution::Eevee => "Eevee".to_string(),
+            EeveeEvolution::Vaporeon(_) => "Vaporeon".to_string(),
+            EeveeEvolution::Jolteon(_) => "Jolteon".to_string(),
+            EeveeEvolution::Flareon(_) => "Flareon".to_string(),
+        }
+    }
+
     /// This method must accomplish the following:
     /// - If the current state is Eevee, evolve into a random evolution.
     /// - If the current state is an evolution (not Eevee), do nothing.
@@ -98,18 +118,26 @@ impl Eevee {
     /// - If the level is greater 10, evolve into Flareon.
     /// The change in state will also change the pokemon's base stats by
     /// a randomly generated multiplier between 0.5 and 1.75
-    /// We'll use the rand crate to generate a random number to determine which evolution to evolve into.
-    /// To add the rand crate to your program run `cargo add rand` in the root directory of your project.
-    /// Consult https://rust-lang-nursery.github.io/rust-cookbook/algorithms/randomness.html for more information.
-    pub fn evolve(&mut self) -> Eevee {
-        todo!()
+    pub fn evolve(&mut self, evolution: EeveeEvolution) {
+        if self.evolution != EeveeEvolution::Eevee {
+            panic!("Tried to evolve an already evolved Eevee");
+        }
+        match evolution {
+            EeveeEvolution::Eevee => panic!("Tried to evolve an Eevee into another Eevee"),
+            EeveeEvolution::Vaporeon(_) => todo!(),
+            EeveeEvolution::Jolteon(_) => todo!(),
+            EeveeEvolution::Flareon(_) => todo!(),
+        }
     }
 
     // Here we want to implement the devolve method for Eevee.
     // This method must accomplish the following:
     // - If the current state is Eevee, do nothing.
     // - If the current state is an evolution (not Eevee), devolve into Eevee.
-    pub fn devolve(&mut self) -> Eevee {
-        todo!()
+    pub fn devolve(&mut self) {
+        if self.evolution == EeveeEvolution::Eevee {
+            panic!("Tried to devolve an Eevee");
+        }
+        self.evolution = EeveeEvolution::Eevee;
     }
 }
