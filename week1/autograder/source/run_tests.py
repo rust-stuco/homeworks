ad9ce14393418dc9455fddf28790b581c44b6453
Connor Tsui <connor.tsui20@gmail.com>
2023-12-24 18:14:59 -0500

    add clippy test lint

--- week1/autograder/source/run_tests.py
+++ week1/autograder/source/run_tests.py
@@ -7,8 +7,9 @@ if __name__ == "__main__":
     # Gather all of the tests in the /autograder/source/tests/ directory
     suite = unittest.defaultTestLoader.discover("tests")
 
+    # Gradescope needs json results written to this exact path
     with open("/autograder/results/results.json", "w") as f:
-        # All tests need to run with the test crate as the root
+        # All cargo tests need to run with the test crate as the root
         os.chdir("/autograder/source/primerlab")
         # Run all tests and send json output to results.json
         JSONTestRunner(visibility="visible", stream=f).run(suite)
--- week1/autograder/source/tests/test_primerlab.py
+++ week1/autograder/source/tests/test_primerlab.py
@@ -1,51 +1,70 @@
 import subprocess
 import unittest
-from gradescope_utils.autograder_utils.decorators import weight
+from gradescope_utils.autograder_utils.decorators import weight, number
+
+
+def verify_output_errors(output):
+    return "error" not in output and "FAILED" not in output
+
+
+def verify_output_warnings(output):
+    return "warning" not in output
 
 
 class PrimerLabTest(unittest.TestCase):
-    def run_cargo_test(self, test_name, cmd):
-        print(f"Testing {test_name}...")
+    def run_cargo_test(self, cmd, verify=verify_output_errors):
         # Runs `cargo test` in a subprocess
         test = subprocess.Popen(
             cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
         )
-        # Capture the output of the subprocess test
+
+        # Capture the output of the subprocess command
         output = test.stdout.read().strip().lower().decode()
         # Show student the output of the test
         print(output)
-        # Check for any errors in output
-        if "error" in output or "test result: FAILED" in output:
-            self.assertTrue(False)
 
+        # Check for any errors in output
+        if not verify(output):
+            self.fail("Error detected! Please review the above to see what went wrong.")
+
+    @number(0.0)
+    @weight(0)
+    def test_clippy(self):
+        """Testing lints"""
+        self.run_cargo_test("cargo clippy", verify_output_warnings)
+
+    @number(1.0)
     @weight(48)
     def test_compiles(self):
         """Testing compilation of exercises"""
-        self.run_cargo_test("compilation", "cargo test exercises::")
+        self.run_cargo_test("cargo test exercises::")
 
+    @number(1.1)
     @weight(2)
     def test_it_works(self):
         """Testing compilation of functions"""
-        self.run_cargo_test("it_works", "cargo test it_works")
+        self.run_cargo_test("cargo test it_works")
 
+    @number(2.0)
     @weight(12)
     def test_is_prime(self):
         """Testing is_prime"""
-        self.run_cargo_test(
-            "is_prime", "cargo test is_prime && cargo test random_primes"
-        )
+        self.run_cargo_test("cargo test is_prime && cargo test random_primes")
 
+    @number(2.1)
     @weight(16)
     def test_nth_prime(self):
         """Testing nth_prime"""
-        self.run_cargo_test("nth_prime", "cargo test --release nth_prime")
+        self.run_cargo_test("cargo test --release nth_prime")
 
+    @number(2.2)
     @weight(11)
     def test_gcd(self):
         """Testing gcd"""
-        self.run_cargo_test("gcd", "cargo test gcd")
+        self.run_cargo_test("cargo test gcd")
 
+    @number(2.3)
     @weight(11)
     def test_fib(self):
         """Testing fib"""
-        self.run_cargo_test("fib", "cargo test fib")
+        self.run_cargo_test("cargo test fib")
