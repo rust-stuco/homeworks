65eb46a3a7dee24f0a9f6b02aa8a5ac7790ea023
Connor Tsui <connor.tsui20@gmail.com>
2023-12-23 21:14:17 -0500

    update build scripts for zip weirdness

--- week1/primerlab_ref/build.rs
+++ week1/primerlab_ref/build.rs
@@ -1,16 +1,17 @@
 use std::process::Command;
 
 /// Add all patterns here
-const PATTERNS: [&str; 2] = ["src/exercises/fixme*.rs", "src/functions.rs"];
-
-/// Output tar handin file
-const HANDIN: &str = "handin.zip";
+const PATTERNS: [&str; 2] = ["src/exercises", "src/functions.rs"];
 
 fn main() {
+    // For some reason zip doesn't like wildcards when run through this
+    // So we have to include the entire src/exercises directory and then exclude the mod.rs file
     Command::new("zip")
         .arg("-r")
-        .arg(HANDIN)
+        .arg("handin.zip")
         .args(PATTERNS)
+        .arg("-x")
+        .arg("src/exercises/mod.rs")
         .output()
         .expect("Unable to zip handin files");
 }
