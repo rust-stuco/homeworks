47651a6460c886c5a92e3ddf963756460c483eb6
Connor Tsui <connor.tsui20@gmail.com>
2023-12-26 13:06:20 -0500

    fix build typo

--- week3/pokelab/build.rs
+++ week3/pokelab/build.rs
@@ -5,6 +5,7 @@ const PATTERNS: [&str; 2] = ["src/pokemon/charmander.rs", "src/pokemon/eevee.rs"
 
 fn main() {
     Command::new("zip")
+        .arg("-r")
         .arg("handin.zip")
         .args(PATTERNS)
         .output()
