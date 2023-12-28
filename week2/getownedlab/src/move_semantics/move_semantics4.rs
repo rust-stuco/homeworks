4009cecae1e967c1a9b84ede3eb9de8ae967b83b
Connor Tsui <connor.tsui20@gmail.com>
2023-12-27 20:47:15 -0500

    add handout

---
+++ week2/getownedlab/src/move_semantics/move_semantics4.rs
@@ -0,0 +1,12 @@
+/// Make me compile by ONLY reordering the lines in `main()`,
+/// but without adding, changing or removing any of them.
+
+#[test]
+fn get_in_line() {
+    let mut x = 100;
+    let y = &mut x;
+    let z = &mut x;
+    *y += 100;
+    *z += 1000;
+    assert_eq!(x, 1200);
+}
---
+++ week2/getownedlab/src/strings/mod.rs
@@ -0,0 +1,11 @@
+// All examples taken from [
+// rustlings exercises](https://github.com/rust-lang/rustlings/tree/main/exercises/move_semantics)
+//
+// Uncomment each of these and ensure that it compiles with `cargo test`.
+// We recommend uncommenting them one at a time.
+// Make sure you read ALL of the error messages! They contain VERY useful information.
+
+// pub mod strings1;
+// pub mod strings2;
+// pub mod strings3;
+// pub mod strings4;
