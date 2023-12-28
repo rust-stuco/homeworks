a1de58ea6b30e3603be42ecb897753c8842126e5
Connor Tsui <connor.tsui20@gmail.com>
2023-12-27 19:13:57 -0500

    fixed build issue

--- week1/primerlab_ref/build.rs
+++ week1/primerlab_ref/build.rs
@@ -1,17 +1,14 @@
 use std::process::Command;
 
-/// Add all patterns here
-const PATTERNS: [&str; 2] = ["src/exercises", "src/functions.rs"];
-
 fn main() {
-    // For some reason zip doesn't like wildcards when run through this
-    // So we have to include the entire src/exercises directory and then exclude the mod.rs file
-    Command::new("zip")
-        .arg("-r")
-        .arg("handin.zip")
-        .args(PATTERNS)
-        .arg("-x")
-        .arg("src/exercises/mod.rs")
-        .output()
-        .expect("Unable to zip handin files");
+    if cfg!(unix) {
+        Command::new("zip")
+            .arg("-r")
+            .arg("handin.zip")
+            .arg("src/")
+            .output()
+            .expect("Unable to zip handin files");
+    }
+
+    println!("cargo:rerun-if-changed=handin.zip");
 }
