dc53f37ef769d77c0280fa5ca10c23b7041333a2
Connor Tsui <connor.tsui20@gmail.com>
2023-12-31 15:57:20 -0500

    changed values to just vec

--- week4/multilab_ref/src/multimap.rs
+++ week4/multilab_ref/src/multimap.rs
@@ -1,43 +1,11 @@
-use crate::multiset::MultiSet;
 use std::collections::HashMap;
 use std::hash::Hash;
 
-/// Trait Alias for all types that implement these traits
-pub trait Value: Eq + Hash {}
-
-#[non_exhaustive]
-pub enum Values<V: Value> {
-    SmallSet(Vec<V>),
-    LargeSet(MultiSet<V>),
+pub struct MultiMap<K: Hash + Eq, V: Eq> {
+    inner: HashMap<K, Vec<V>>,
 }
 
-impl<V: Value> Values<V> {
-    pub fn contains(&self, key: &V) -> bool {
-        match self {
-            Self::SmallSet(v) => v.contains(key),
-            Self::LargeSet(ms) => ms.contains(key),
-        }
-    }
-
-    fn transform_large(&mut self) {
-        match self {
-            Self::LargeSet(_) => (),
-            Self::SmallSet(vs) => {
-                let mut ms = MultiSet::new();
-                while let Some(element) = vs.pop() {
-                    ms.insert(element);
-                }
-                *self = Self::LargeSet(ms);
-            }
-        }
-    }
-}
-
-pub struct MultiMap<K: Value, V: Value> {
-    inner: HashMap<K, Values<V>>,
-}
-
-impl<K: Value, V: Value> MultiMap<K, V> {
+impl<K: Hash + Eq, V: Eq> MultiMap<K, V> {
     pub fn new() -> Self {
         Self {
             inner: HashMap::new(),
@@ -45,50 +13,36 @@ impl<K: Value, V: Value> MultiMap<K, V> {
     }
 
     pub fn insert(&mut self, key: K, value: V) {
-        let values = self.inner.entry(key).or_insert(Values::SmallSet(vec![]));
-
-        if let Values::SmallSet(vs) = values {
-            if vs.len() >= 98 {
-                values.transform_large();
-            }
-        }
-
-        match values {
-            Values::SmallSet(vs) => vs.push(value),
-            Values::LargeSet(ms) => ms.insert(value),
-        }
+        self.inner.entry(key).or_default().push(value);
     }
 
     pub fn remove_value(&mut self, key: &K, value: &V) -> bool {
         match self.inner.get_mut(key) {
             None => false,
-            Some(values) => match values {
-                Values::SmallSet(vs) => match vs.iter().position(|x| x == value) {
-                    None => false,
-                    Some(i) => {
-                        vs.remove(i);
-                        true
-                    }
-                },
-                Values::LargeSet(ms) => ms.remove(value),
+            Some(values) => match values.iter().position(|x| x == value) {
+                None => false,
+                Some(i) => {
+                    values.remove(i);
+                    true
+                }
             },
         }
     }
 
-    pub fn remove_key(&mut self, key: &K) -> Option<Values<V>> {
+    pub fn remove_key(&mut self, key: &K) -> Option<Vec<V>> {
         self.inner.remove(key)
     }
 
-    pub fn get_values(&self, key: &K) -> Option<&Values<V>> {
+    pub fn get_values(&self, key: &K) -> Option<&Vec<V>> {
         self.inner.get(key)
     }
 
-    pub fn get_values_mut(&mut self, key: &K) -> Option<&mut Values<V>> {
+    pub fn get_values_mut(&mut self, key: &K) -> Option<&mut Vec<V>> {
         self.inner.get_mut(key)
     }
 }
 
-impl<K: Value, V: Value> Default for MultiMap<K, V> {
+impl<K: Hash + Eq, V: Eq> Default for MultiMap<K, V> {
     fn default() -> Self {
         Self::new()
     }
