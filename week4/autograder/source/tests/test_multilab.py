17065b9c77d6ec98631d2c79bca71806aaa20cd5
Connor Tsui <connor.tsui20@gmail.com>
2024-01-08 22:01:56 -0500

    add all tests

--- week4/autograder/source/tests/test_multilab.py
+++ week4/autograder/source/tests/test_multilab.py
@@ -77,7 +77,57 @@ class MultiLabTest(unittest.TestCase):
                 "Please fix the lints above to receive credit for this assignment:\n"
             )
 
-    @cargo_test(1.0, 100)
-    def test_all_tests(self):
-        """Testing all tests"""
-        return "cargo test"
+    @cargo_test(1.0, 7)
+    def test_multiset_docs(self):
+        """Testing multiset doc tests"""
+        return "cargo test multiset --doc"
+
+    @cargo_test(1.1, 8)
+    def test_multiset_basic(self):
+        """Testing multiset basic"""
+        return "cargo test multiset_tests::test_insert_remove_basic"
+
+    @cargo_test(1.2, 10)
+    def test_multiset_more(self):
+        """Testing multiset more"""
+        return "cargo test multiset_tests::test_insert_remove_more"
+
+    @cargo_test(1.3, 10)
+    def test_multiset_multiple(self):
+        """Testing multiset multiple"""
+        return "cargo test multiset_tests::test_multiple_insertions_and_removals"
+
+    @cargo_test(1.4, 15)
+    def test_multiset_interleaved(self):
+        """Testing multiset interleaved"""
+        return "cargo test multiset_tests::test_interleaved_insert_remove"
+
+    @cargo_test(2.0, 6)
+    def test_multimap_docs(self):
+        """Testing multimap doc tests"""
+        return "cargo test multimap --doc"
+
+    @cargo_test(2.1, 6)
+    def test_multimap_duplicate(self):
+        """Testing multimap duplicates"""
+        return "cargo test multimap_tests::test_insert_duplicate_values"
+
+    @cargo_test(2.2, 6)
+    def test_multimap_many(self):
+        """Testing multimap many"""
+        return "cargo test multimap_tests::test_insert_many_values"
+
+    @cargo_test(2.3, 6)
+    def test_multimap_not_exist(self):
+        """Testing multimap not exist"""
+        return "cargo test multimap_tests::test_remove_nonexistent_key"
+
+    @cargo_test(2.4, 6)
+    def test_multimap_remove(self):
+        """Testing multimap remove"""
+        return "cargo test multimap_tests::test_remove_value_from_multiple"
+
+    @cargo_test(2.5, 20)
+    def test_multimap_integrated(self):
+        """Testing multimap integrated"""
+        return "cargo test multimap_tests::test_large_insert_remove"
