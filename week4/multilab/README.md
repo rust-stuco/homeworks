bac59ac8a4aee4cd7b9cdf78d421f8cc44404c62
Connor Tsui <connor.tsui20@gmail.com>
2024-01-08 22:09:22 -0500

    add handout

---
+++ week4/multilab/README.md
@@ -0,0 +1,86 @@
+### 98-008: Intro to the Rust Programming Language
+
+
+
+# Multi Lab
+
+The goal of this homework is to make sure you are comfortable with some of the
+generic collection types in the [`std::collections`] module.
+
+You will implement two data structures that track multiplicity on top of their normal operations.
+The first will be a [`MultiSet`](crate::multiset::MultiSet), or a set that can have duplicates.
+The second will be a [`MultiMap`](crate::multimap::MultiMap), which is a map that maps keys
+to any number of values.
+
+For [`MultiSet`](crate::multiset::MultiSet), we have not provided what types the fields should be.
+_We recommend that you use one of the collection types provided in the_
+_[`std::collections`] module to help you build these new types._
+
+This homework is a bit more straightforward than the previous homeworks, so we will be providing
+less detail than before on implementation details. Instead, you should read the documentation
+and implement the data structure and methods so that they follow the documentation.
+
+**You will receive full points as long as you pass all of the test cases.**
+
+
+
+# Part 1: `MultiSet`
+
+Click this hyperlink to go to the documentation for [`MultiSet`](crate::multiset::MultiSet)!
+
+
+
+# Part 2: `MultiMap`
+
+Click this hyperlink to go to the documentation for [`MultiMap`](crate::multimap::MultiMap)!
+
+
+
+# Submission
+
+
+### Unix
+
+If you are on a unix system, we will try to create a `handin.zip` automatically for you,
+**but you will need to have `zip` already installed**.
+
+If you _do not_ have `zip` installed on your system,
+install `zip` on your machine or use the CMU Linux SSH machines.
+If you need help with this, please reach out to us!
+
+Once you have `zip` installed, we will create the `handin.zip` automatically for you
+(_take a peek into `build.rs` if you're interested in how this works!_).
+
+Once you have the `handin.zip` file, submit it to Gradescope.
+
+
+### Windows
+
+If you are on a windows system, you can zip the `src/` folder manually
+and upload that to Gradescope.
+
+Note that you don't _need_ to name it `handin.zip`, you can name it whatever you'd like.
+
+
+
+# Collaboration
+
+In general, feel free to discuss homeworks with other students!
+As long as you do not copy someone else's work, any communication is fair game.
+
+Try to discuss on the course Discord or Piazza so that
+other students can see your questions and answers as well!
+
+Remember that all formal questions should be asked on Piazza.
+
+
+
+# Feedback
+
+We would like to reiterate that you should let us know if you spent
+anywhere in significant excess of an hour on this homework.
+
+In addition, Rust has a notoriously steep learning curve,
+so if you find yourself not understanding the concepts,
+you should reach out to us and let us know as well ---
+chances are, you're not the only one!
---
+++ week4/multilab/src/multimap.rs
@@ -0,0 +1,163 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+
+/// A multimap that associates keys with multiple values.
+///
+/// This structure is similar to a [`HashMap`], but it allows multiple values to be
+/// associated with a single key, instead of the usual one-to-one mapping.
+/// It is implemented using a [`HashMap`] where each
+/// value is a [`Vec`] of the associated values.
+///
+/// Note that unlike [`MultiSet`](crate::multiset::MultiSet), it is fine to expose the
+/// inner hash table here because we are not storing any other metadata
+/// (in the previous section, you probably had more than one field tracking data).
+/// This means that an outsider can't mess up our data structure by breaking any invariants.
+/// So this type is really just a wrapper around the inner data structure
+/// that provides some useful methods.
+///
+/// # Examples
+///
+/// ```rust
+/// # use multilab::multimap::MultiMap;
+/// #
+/// let mut multimap = MultiMap::new();
+/// multimap.insert(1, "hello");
+/// multimap.insert(1, "world");
+/// multimap.insert(2, "foo");
+///
+/// assert_eq!(multimap.get_values(&1).unwrap(), &["hello", "world"]);
+/// assert_eq!(multimap.get_values(&2).unwrap(), &["foo"]);
+/// ```
+///
+/// ### `Eq + Hash`
+/// See the note in the documentation for [`MultiSet`](crate::multiset::MultiSet).
+#[derive(Debug)]
+pub struct MultiMap<K: Hash + Eq, V: Eq> {
+    /// The internal [`HashMap`] storing the key-value associations.
+    pub inner: HashMap<K, Vec<V>>,
+}
+
+impl<K: Hash + Eq, V: Eq> MultiMap<K, V> {
+    /// Creates a new, empty [`MultiMap`].
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    /// Inserts a key-value pair into the [`MultiMap`].
+    ///
+    /// If the key already exists, the value is added to the existing vector of values.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use multilab::multimap::MultiMap;
+    /// #
+    /// let mut multimap = MultiMap::new();
+    /// multimap.insert(1, "hello");
+    /// multimap.insert(1, "world");
+    ///
+    /// assert_eq!(multimap.get_values(&1).unwrap(), &["hello", "world"]);
+    /// ```
+    pub fn insert(&mut self, key: K, value: V) {
+        todo!()
+    }
+
+    /// Removes a specific value from the values associated with a key.
+    ///
+    /// Returns `true` if the value was removed, `false` if the key or value was not found.
+    ///
+    /// Note that removing all values of a key will remove the key itself and the
+    /// `Vec<V>` mapped to it.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use multilab::multimap::MultiMap;
+    /// #
+    /// let mut multimap: MultiMap<i32, &'static str> = MultiMap::new();
+    /// multimap.insert(1, "hello");
+    /// multimap.insert(1, "world");
+    ///
+    /// assert!(multimap.remove_value(&1, &"hello"));
+    /// assert_eq!(multimap.get_values(&1).unwrap(), &["world"]);
+    /// ```
+    pub fn remove_value(&mut self, key: &K, value: &V) -> bool {
+        todo!()
+    }
+
+    /// Removes all values associated with a key and returns them.
+    ///
+    /// Returns `None` if the key was not found.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use multilab::multimap::MultiMap;
+    /// #
+    /// let mut multimap = MultiMap::new();
+    /// multimap.insert(1, "hello");
+    /// multimap.insert(1, "world");
+    ///
+    /// assert_eq!(multimap.remove_key(&1).unwrap(), &["hello", "world"]);
+    /// assert!(multimap.get_values(&1).is_none());
+    /// ```
+    pub fn remove_key(&mut self, key: &K) -> Option<Vec<V>> {
+        todo!()
+    }
+
+    /// Gets the values associated with a key, if any.
+    ///
+    /// Returns a reference to the vector of values if the key exists, otherwise `None`.
+    /// The returned reference is immutable and can't be modified.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use multilab::multimap::MultiMap;
+    /// #
+    /// let mut multimap = MultiMap::new();
+    /// multimap.insert(1, "hello");
+    ///
+    /// assert_eq!(multimap.get_values(&1).unwrap(), &["hello"]);
+    /// ```
+    ///
+    /// # Challenge
+    ///
+    /// As an exercise, see if you can convert the return type to be `Option<&[V]>` instead.
+    /// All of the tests cases should still be able to compile!
+    pub fn get_values(&self, key: &K) -> Option<&[V]> {
+        todo!()
+    }
+
+    /// Gets a mutable reference to the values associated with a key, if any.
+    ///
+    /// Returns a mutable reference to the vector of values if the key exists, otherwise `None`.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use multilab::multimap::MultiMap;
+    /// #
+    /// let mut multimap = MultiMap::new();
+    /// multimap.insert(1, "hello");
+    ///
+    /// if let Some(values) = multimap.get_values_mut(&1) {
+    ///     values.push("world");
+    /// }
+    ///
+    /// assert_eq!(multimap.get_values(&1).unwrap(), &["hello", "world"]);
+    /// ```
+    pub fn get_values_mut(&mut self, key: &K) -> Option<&mut Vec<V>> {
+        todo!()
+    }
+}
+
+impl<K, V> Default for MultiMap<K, V>
+where
+    K: Eq + Hash,
+    V: Eq,
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
