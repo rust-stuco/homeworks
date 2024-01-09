bdda7adcf468e0c6bf8b23e9d925bda5ed41a7b0
Connor Tsui <connor.tsui20@gmail.com>
2024-01-08 21:39:37 -0500

    week4 autograder first commit

---
+++ week4/autograder/source/multilab/src/lib.rs
@@ -0,0 +1,8 @@
+/// This module contains a multiset type that can store elements of a generic type.
+pub mod multiset;
+
+/// This module contains a multimap type that associates keys with multiple values.
+pub mod multimap;
+
+#[cfg(test)]
+mod tests;
