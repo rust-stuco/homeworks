c2a96506c32a9274eedcc3d3807d92217cceb80d
Connor Tsui <connor.tsui20@gmail.com>
2024-01-18 11:46:17 -0500

    add autograder

---
+++ week5/autograder/source/tests/test_summarylab.py
@@ -0,0 +1,83 @@
+import os, subprocess
+import unittest
+from functools import wraps
+from gradescope_utils.autograder_utils.decorators import weight, number, partial_credit
+
+
+# Main decorator for gradescope tests
+def cargo_test(test_num, weight):
+    def cargo_test_wrapper(func):
+        @number(test_num)
+        @partial_credit(weight)
+        @wraps(func)
+        def test(self, set_score=None):
+            if not self.passed_clippy:
+                set_score(0)
+                print(
+                    "Detected warnings and/or errors in cargo clippy output! "
+                    "Setting score to 0 and moving on to tests:\n"
+                )
+            else:
+                print("cargo clippy succeeded, moving on to tests:\n")
+
+            # Run the command and show student the output
+            cmd = func(self)
+            print(f"Running `{cmd}`...\n")
+            output = run_cmd(cmd)
+            print(output)
+
+            # Check for any errors in output
+            if not verify_output_errors(output):
+                self.fail(
+                    "Error detected! Please review the above to see what went wrong."
+                )
+
+        return test
+
+    return cargo_test_wrapper
+
+
+def verify_output_errors(output):
+    return "error" not in output and "FAILED" not in output
+
+
+def verify_output_warnings(output):
+    return "warning" not in output and verify_output_errors(output)
+
+
+# Runs given shell command in a subprocess
+def run_cmd(cmd):
+    test = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
+    # Capture the output of the subprocess command
+    output = test.stdout.read().strip().lower().decode()
+    return output
+
+
+class SummaryLabTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Need to cd into crate root here,
+        # for some reason it initializes before the os.chdir in run_tests.py
+        os.chdir("/autograder/source/summarylab")
+
+        self.clippy_output = run_cmd("cargo clippy")
+        self.passed_clippy = verify_output_warnings(self.clippy_output)
+
+    @number(0.0)
+    @weight(0)
+    def test_clippy_check(self):
+        """Testing cargo clippy"""
+        print(self.clippy_output)
+        if not self.passed_clippy:
+            self.fail(
+                "Detected warnings and/or errors in cargo clippy output!\n"
+                "Please fix the lints above to receive credit for this assignment:\n"
+            )
+
+    @cargo_test(1.0, 100)
+    def test_all_tests(self):
+        """Testing all tests"""
+        return "cargo test"
