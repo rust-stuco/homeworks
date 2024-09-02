56bc0bdf0664ab977549237a7bec3318fa809204
Connor Tsui <connor.tsui20@gmail.com>
2024-09-02 09:39:16 -0400

    make week2 file comments module-level

--- week2/getownedlab/src/slices/mod.rs
+++ week2/getownedlab/src/slices/mod.rs
@@ -1,8 +1,8 @@
-// All examples taken from [practice.rs](https://practice.rs/compound-types/slice.html)
-//
-// Uncomment each of these and ensure that it compiles with `cargo test`.
-// We recommend uncommenting them one at a time.
-// Make sure you read ALL of the error messages! They contain VERY useful information.
+//! All examples taken from [practice.rs](https://practice.rs/compound-types/slice.html)
+//!
+//! Uncomment each of these and ensure that it compiles with `cargo test`.
+//! We recommend uncommenting them one at a time.
+//! Make sure you read ALL of the error messages! They contain VERY useful information.
 
 pub mod slices1;
 // pub mod slices2;
