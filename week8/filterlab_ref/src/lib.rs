124f33d06a05f56d3379d5f19af1e0999ce4c36b
Connor Tsui <connor.tsui20@gmail.com>
2024-10-05 14:33:26 -0400

    undo

--- week8/filterlab_ref/src/lib.rs
+++ week8/filterlab_ref/src/lib.rs
@@ -1,16 +1,19 @@
-pub fn fibonacci(n: u64) -> u64 {
-    let mut a = 0;
-    let mut b = 1;
+use bitvec::prelude as bv;
+use std::marker::PhantomData;
 
-    match n {
-        0 => b,
-        _ => {
-            for _ in 0..n {
-                let c = a + b;
-                a = b;
-                b = c;
-            }
-            b
-        }
-    }
+/// An approximate-membership query / probabilistic data structure that supports point lookups.
+pub struct BloomFilter<T> {
+    /// The inner bitvector / bitset that keeps track of our hashed values.
+    ///
+    /// Note that you are allowed to implement your own bitvector if you would prefer!
+    bitvector: bv::BitVec,
+
+    /// A type marker used to express that this `BloomFilter` is generic over a single type rather
+    /// than allowing it to store multiple different types.
+    ///
+    /// Note that we could have gone with the approach of making only hashing generic, but for
+    /// simplicity we will only allow the `BloomFilter` to track elements of a single type.
+    phantom: PhantomData<T>,
 }
+
+impl BloomFilter {}
