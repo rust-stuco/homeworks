606bd6f1eceba15a99397060553d2ff7d09e0d71
Connor Tsui <connor.tsui20@gmail.com>
2024-10-05 14:21:19 -0400

    testing

--- week8/filterlab_ref/src/lib.rs
+++ week8/filterlab_ref/src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub fn fibonacci(n: u64) -> u64 {
+    let mut a = 0;
+    let mut b = 1;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    match n {
+        0 => b,
+        _ => {
+            for _ in 0..n {
+                let c = a + b;
+                a = b;
+                b = c;
+            }
+            b
+        }
     }
 }
