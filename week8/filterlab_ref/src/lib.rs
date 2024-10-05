07cbfeb1e5bc6f4d4805730e374ed08966bb75a2
Connor Tsui <connor.tsui20@gmail.com>
2024-10-05 14:49:16 -0400

    base

--- week8/filterlab_ref/src/lib.rs
+++ week8/filterlab_ref/src/lib.rs
@@ -1,4 +1,5 @@
 use bitvec::prelude as bv;
+use std::hash::Hash;
 use std::marker::PhantomData;
 
 /// An approximate-membership query / probabilistic data structure that supports point lookups.
@@ -8,6 +9,9 @@ pub struct BloomFilter<T> {
     /// Note that you are allowed to implement your own bitvector if you would prefer!
     bitvector: bv::BitVec,
 
+    /// Several statistics related to the bloom filter data structure.
+    stats: BloomFilterStats,
+
     /// A type marker used to express that this `BloomFilter` is generic over a single type rather
     /// than allowing it to store multiple different types.
     ///
@@ -16,4 +20,44 @@ pub struct BloomFilter<T> {
     phantom: PhantomData<T>,
 }
 
-impl BloomFilter {}
+struct BloomFilterStats {
+    /// The total number of bits in the bloom filter's bitvector.
+    num_bits: usize,
+
+    /// The maximum number of elements that this bloom filter is allowed to store to maintain its
+    /// theoretical false positive rate.
+    max_elements: usize,
+
+    /// The approximate current number of elements that this bloom filter is storing.
+    curr_elements: usize,
+
+    /// The number of hash functions per element.
+    num_hashes: usize,
+}
+
+impl<T: Hash> BloomFilter<T> {
+    /// Creates a new `BloomFilter` given the maximum number of elements that will be inserted into
+    /// the filter and a bound on the false positive rate.
+    pub fn new_with_fpr(max_elements: usize, fpr: f64) -> Self {
+        todo!()
+    }
+
+    /// Creates a new `BloomFilter` given the maximum number of elements that will be inserted into
+    /// the filter and a bound on the size of the `BloomFilter`'s bitvector
+    pub fn new_with_size(max_elements: usize, num_bits: usize) -> Self {
+        todo!()
+    }
+
+    /// Inserts an element into the bloom filter.
+    ///
+    /// Returns `true` if the element might have been in the bloom filter previously before
+    /// insertion.
+    pub fn insert(&mut self, elem: &T) -> bool {
+        todo!()
+    }
+
+    /// Checks if an element might have been previously inserted into the bloom filter.
+    pub fn contains(&mut self, elem: &T) -> bool {
+        todo!()
+    }
+}
