8667084b38130c9aede9f5b2bebbb0e6babaed61
Connor Tsui <connor.tsui20@gmail.com>
2024-10-05 23:21:11 -0400

    naive implementation done

--- week8/filterlab_ref/src/lib.rs
+++ week8/filterlab_ref/src/lib.rs
@@ -1,5 +1,5 @@
 use bitvec::prelude as bv;
-use std::hash::Hash;
+use std::hash::{DefaultHasher, Hash, Hasher};
 use std::marker::PhantomData;
 
 /// An approximate-membership query / probabilistic data structure that supports point lookups.
@@ -10,7 +10,7 @@ pub struct BloomFilter<T> {
     bitvector: bv::BitVec,
 
     /// Several statistics related to the bloom filter data structure.
-    stats: BloomFilterStats,
+    num_hashes: usize,
 
     /// A type marker used to express that this `BloomFilter` is generic over a single type rather
     /// than allowing it to store multiple different types.
@@ -20,44 +20,61 @@ pub struct BloomFilter<T> {
     phantom: PhantomData<T>,
 }
 
-struct BloomFilterStats {
-    /// The total number of bits in the bloom filter's bitvector.
-    num_bits: usize,
-
-    /// The maximum number of elements that this bloom filter is allowed to store to maintain its
-    /// theoretical false positive rate.
-    max_elements: usize,
-
-    /// The approximate current number of elements that this bloom filter is storing.
-    curr_elements: usize,
-
-    /// The number of hash functions per element.
-    num_hashes: usize,
-}
-
 impl<T: Hash> BloomFilter<T> {
     /// Creates a new `BloomFilter` given the maximum number of elements that will be inserted into
-    /// the filter and a bound on the false positive rate.
-    pub fn new_with_fpr(max_elements: usize, fpr: f64) -> Self {
-        todo!()
-    }
-
-    /// Creates a new `BloomFilter` given the maximum number of elements that will be inserted into
-    /// the filter and a bound on the size of the `BloomFilter`'s bitvector
-    pub fn new_with_size(max_elements: usize, num_bits: usize) -> Self {
-        todo!()
+    /// the filter and a bound on the size of the `BloomFilter`'s bitvector.
+    pub fn new(num_bits: usize, num_hashes: usize) -> Self {
+        Self {
+            bitvector: bv::BitVec::with_capacity(num_bits),
+            num_hashes,
+            phantom: PhantomData,
+        }
     }
 
     /// Inserts an element into the bloom filter.
     ///
-    /// Returns `true` if the element might have been in the bloom filter previously before
-    /// insertion.
-    pub fn insert(&mut self, elem: &T) -> bool {
-        todo!()
+    /// Note that this implementation is purposefully slow. We would like you to think of ways to
+    /// improve the performance!
+    pub fn insert(&mut self, elem: &T) {
+        let size = self.bitvector.len();
+
+        let mut hasher = DefaultHasher::new();
+
+        // Provide a starting "seed" for hashing.
+        elem.hash(&mut hasher);
+        let mut hash = hasher.finish();
+
+        for _ in 0..self.num_hashes {
+            // Rehash the hash.
+            hash.hash(&mut hasher);
+            hash = hasher.finish();
+
+            // Set the bit corresponding to this hash value.
+            self.bitvector.set(hash as usize % size, true);
+        }
     }
 
     /// Checks if an element might have been previously inserted into the bloom filter.
     pub fn contains(&mut self, elem: &T) -> bool {
-        todo!()
+        let size = self.bitvector.len();
+
+        let mut hasher = DefaultHasher::new();
+
+        // Provide a starting "seed" for hashing.
+        elem.hash(&mut hasher);
+        let mut hash = hasher.finish();
+
+        for _ in 0..self.num_hashes {
+            // Rehash the hash.
+            hash.hash(&mut hasher);
+            hash = hasher.finish();
+
+            // Set the bit corresponding to this hash value.
+            if self.bitvector.get(hash as usize % size).is_none() {
+                return false;
+            }
+        }
+
+        true
     }
 }
