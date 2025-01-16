6dacfa800787e7916b402df5cb88f7ddbe1e2df1
Connor Tsui <connor.tsui20@gmail.com>
2025-01-16 16:06:23 -0500

    clean up all autograders

--- week8/autograder/source/run_tests.py
+++ week8/autograder/source/run_tests.py
@@ -4,13 +4,13 @@ from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 
 if __name__ == "__main__":
-    # Gather all of the tests in the /autograder/source/tests/ directory
+    # Gather all of the tests in the `/autograder/source/tests/` directory.
     suite = unittest.defaultTestLoader.discover("tests")
 
-    # All cargo tests need to run with the test crate as the root
+    # All cargo tests need to run with the test crate as the root.
     os.chdir("/autograder/source/iterlab")
 
-    # Gradescope needs json results written to this exact path
+    # Gradescope needs JSON results written to this exact path.
     with open("/autograder/results/results.json", "w") as f:
-        # Run all tests and send json output to results.json
+        # Run all tests and send JSON output to `results.json`.
         JSONTestRunner(visibility="visible", stream=f).run(suite)
