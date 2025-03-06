9cb15268c733a3ec2aaf3ddd60b8230f0c30aa25
Connor Tsui <connor.tsui20@gmail.com>
2025-03-06 12:13:44 -0500

    better tests

--- week8/splitlab/src/tests/str.rs
+++ week8/splitlab/src/tests/str.rs
@@ -37,3 +37,52 @@ test_str!(test_no_delimiter_in_haystack, "abcde", "x", vec!["abcde"]);
 test_str!(test_unicode_delimiter, "a😊b😊c", "😊", vec!["a", "b", "c"]);
 
 test_str!(test_only_delimiters, ";;;", ";", vec!["", "", "", ""]);
+
+test_str!(
+    test_empty_haystack_multi_char_delimiter,
+    "",
+    "abc",
+    vec![""]
+);
+
+test_str!(
+    test_ending_delimiter,
+    "a b c ",
+    " ",
+    vec!["a", "b", "c", ""]
+);
+
+test_str!(
+    test_delimiters_with_spaces,
+    "a, b, c",
+    ", ",
+    vec!["a", "b", "c"]
+);
+
+test_str!(
+    test_mixed_delimiters,
+    "a, b; c, d",
+    ", ",
+    vec!["a", "b; c", "d"]
+);
+
+test_str!(
+    test_delimiter_not_in_ascii,
+    "a→b→c",
+    "→",
+    vec!["a", "b", "c"]
+);
+
+test_str!(
+    test_delimiter_with_escapes,
+    "a\nb\nc",
+    "\n",
+    vec!["a", "b", "c"]
+);
+
+test_str!(
+    test_haystack_with_bars,
+    "a||b||c",
+    "||",
+    vec!["a", "b", "c"]
+);
--- week8/splitlab_ref/src/tests/mod.rs
+++ week8/splitlab_ref/src/tests/mod.rs
@@ -10,13 +10,9 @@ macro_rules! test_str {
             let split_result: Vec<_> = Split::new($haystack, $pattern).collect();
             assert_eq!(split_result, $expected);
 
-            // Test with `SplitPattern`.
-            let pattern_result: Vec<_> = SplitPattern::new($haystack, $pattern).collect();
-            assert_eq!(pattern_result, $expected);
-
             // Test against the real `split` method.
             assert_eq!(
-                pattern_result,
+                split_result,
                 $haystack.split($pattern).collect::<Vec<&str>>()
             );
         }
@@ -28,6 +24,7 @@ macro_rules! test_pattern {
     ($name:ident, $haystack:expr, $pattern:expr, $expected:expr) => {
         #[test]
         fn $name() {
+            // Test with `SplitPattern`.
             let pattern_result: Vec<_> = SplitPattern::new($haystack, $pattern).collect();
             assert_eq!(pattern_result, $expected);
 
